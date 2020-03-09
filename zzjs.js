8fb93c1e8678686b45f7d02e7d8280b1c252683f
joshretief <61984189+joshretief@users.noreply.github.com>
2020-03-09 16:14:53 -0500

    Create zzjs.js

---
+++ zzjs.js
@@ -0,0 +1,116 @@
+
+
+
+$(function () {
+    $("#image1").click(function () {
+    $("#mainimage1").addClass("bg1");
+           $("#mainimage1").removeClass("bg3 bg2 bg4 bg5");
+                $("#image2").removeClass("imageselect");
+            $("#image3").removeClass("imageselect");
+            $("#image4").removeClass("imageselect");
+            $("#image5").removeClass("imageselect");
+           $("#image1").addClass("imageselect");
+    });
+});
+$(function () {
+    $("#image2").click(function () {
+      $("#mainimage1").addClass("bg2");
+           $("#mainimage1").removeClass("bg1 bg3 bg4 bg5");
+             $("#image1").removeClass("imageselect");
+            $("#image3").removeClass("imageselect");
+            $("#image4").removeClass("imageselect");
+                         $("#image5").removeClass("imageselect");
+            $("#image2").addClass("imageselect");                     
+    });
+});
+$(function () {
+    $("#image3").click(function () {
+                   $("#mainimage1").addClass("bg3");
+           $("#mainimage1").removeClass("bg1 bg2 bg4 bg5");
+              $("#image1").removeClass("imageselect");
+            $("#image2").removeClass("imageselect");
+            $("#image4").removeClass("imageselect");
+         $("#image5").removeClass("imageselect");
+     $("#image3").addClass("imageselect");         
+    });
+});
+$(function () {
+    $("#image4").click(function () {
+   $("#mainimage1").addClass("bg4");
+           $("#mainimage1").removeClass("bg5 bg3 bg2 bg1");
+          $("#image1").removeClass("imageselect");
+            $("#image2").removeClass("imageselect");
+            $("#image3").removeClass("imageselect");
+             $("#image5").removeClass("imageselect");
+             $("#image4").addClass("imageselect"); 
+    });
+});
+$(function () {
+    $("#image5").click(function () {
+   $("#mainimage1").addClass("bg5");
+           $("#mainimage1").removeClass("bg3 bg2 bg4 bg1");
+           $("#image1").removeClass("imageselect");
+            $("#image2").removeClass("imageselect");
+            $("#image3").removeClass("imageselect");
+            $("#image4").removeClass("imageselect");
+             $("#image5").addClass("imageselect");          
+    });
+});
+
+//secondone
+
+$(function () {
+    $("#image12").click(function () {
+    $("#mainimage2").addClass("bg12");
+           $("#mainimage2").removeClass("bg32 bg22 bg42 bg52");
+                $("#image22").removeClass("imageselect");
+            $("#image32").removeClass("imageselect");
+            $("#image42").removeClass("imageselect");
+            $("#image52").removeClass("imageselect");
+           $("#image12").addClass("imageselect");
+    });
+});
+$(function () {
+    $("#image22").click(function () {
+      $("#mainimage2").addClass("bg22");
+           $("#mainimage2").removeClass("bg12 bg32 bg42 bg52");
+             $("#image12").removeClass("imageselect");
+            $("#image32").removeClass("imageselect");
+            $("#image42").removeClass("imageselect");
+                         $("#image52").removeClass("imageselect");
+            $("#image22").addClass("imageselect");                     
+    });
+});
+$(function () {
+    $("#image32").click(function () {
+                   $("#mainimage2").addClass("bg32");
+           $("#mainimage2").removeClass("bg12 bg22 bg42 bg52");
+              $("#image12").removeClass("imageselect");
+            $("#image22").removeClass("imageselect");
+            $("#image42").removeClass("imageselect");
+         $("#image52").removeClass("imageselect");
+     $("#image32").addClass("imageselect");         
+    });
+});
+$(function () {
+    $("#image42").click(function () {
+   $("#mainimage2").addClass("bg42");
+           $("#mainimage2").removeClass("bg52 bg32 bg22 bg12");
+          $("#image12").removeClass("imageselect");
+            $("#image22").removeClass("imageselect");
+            $("#image32").removeClass("imageselect");
+             $("#image52").removeClass("imageselect");
+             $("#image42").addClass("imageselect"); 
+    });
+});
+$(function () {
+    $("#image52").click(function () {
+   $("#mainimage2").addClass("bg52");
+           $("#mainimage2").removeClass("bg32 bg22 bg42 bg12");
+           $("#image12").removeClass("imageselect");
+            $("#image22").removeClass("imageselect");
+            $("#image32").removeClass("imageselect");
+            $("#image42").removeClass("imageselect");
+             $("#image52").addClass("imageselect");          
+    });
+});
